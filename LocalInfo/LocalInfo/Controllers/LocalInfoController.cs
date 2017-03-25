c6c65575e2dd38490e3e98151392772fef3beecd
Jason Michalowski <jasonmbusiness1985@gmail.com>
2017-03-25 13:36:59 -0700

    renamed controller, removed all endpoints besides get

---
+++ LocalInfo/LocalInfo/Controllers/LocalInfoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocalInfo.Controllers
+{
+    [Route("api/[controller]")]
+    public class LocalInfoController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public object Get()
+        {
+
+
+            return new string[] { "value1", "145616519" };
+        }
+        
+    }
+}
